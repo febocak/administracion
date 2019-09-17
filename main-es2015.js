bc8218aa1232a9167f5f14065e46b4510310ccdc
Julio Pérez <soyJulioPerez@outlook.com>
2019-09-17 18:07:18 -0300

    Auto-generated commit

--- main-es2015.js
+++ main-es2015.js
@@ -462,7 +462,7 @@ __webpack_require__.r(__webpack_exports__);
 
 let AppComponent = class AppComponent {
     constructor() {
-        this.title = 'inscripciones';
+        this.title = 'febocak01';
     }
 };
 AppComponent = tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([
@@ -1346,23 +1346,23 @@ __webpack_require__.r(__webpack_exports__);
 
 const environment = {
     production: true,
-    /* firebaseConfig: {
-      apiKey: "AIzaSyBKM08A9WHEhTLXbY7CxF49gN5fl-hk8f8",
-      authDomain: "febocak-02.firebaseapp.com",
-      databaseURL: "https://febocak-02.firebaseio.com",
-      projectId: "febocak-02",
-      storageBucket: "",
-      messagingSenderId: "850669499931",
-      appId: "1:850669499931:web:53e8e8e339850d33"
-      }, */
     firebaseConfig: {
-        apiKey: "AIzaSyDbLNWzHhl1dUWFohXzkv1U7ymzgAZgy68",
-        authDomain: "fobokac-01.firebaseapp.com",
-        databaseURL: "https://fobokac-01.firebaseio.com",
-        projectId: "fobokac-01",
-        storageBucket: "fobokac-01.appspot.com",
-        messagingSenderId: "1019734820364"
+        apiKey: "AIzaSyBKM08A9WHEhTLXbY7CxF49gN5fl-hk8f8",
+        authDomain: "febocak-02.firebaseapp.com",
+        databaseURL: "https://febocak-02.firebaseio.com",
+        projectId: "febocak-02",
+        storageBucket: "",
+        messagingSenderId: "850669499931",
+        appId: "1:850669499931:web:53e8e8e339850d33"
     },
+    /* firebaseConfig: {
+      apiKey: "AIzaSyDbLNWzHhl1dUWFohXzkv1U7ymzgAZgy68",
+      authDomain: "fobokac-01.firebaseapp.com",
+      databaseURL: "https://fobokac-01.firebaseio.com",
+      projectId: "fobokac-01",
+      storageBucket: "fobokac-01.appspot.com",
+      messagingSenderId: "1019734820364"
+    }, */
     modulo: 'Administración'
 };
 
